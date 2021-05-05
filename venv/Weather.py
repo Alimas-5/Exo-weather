810599d2f081e978003919489fa70d0ae8275451
Ludovic Bourget <ludovic.bourget@live.fr>
2021-05-05 15:48:11 +0200

    Create Gitignore

--- venv/Weather.py
+++ venv/Weather.py
@@ -11,6 +11,7 @@ class weather:
     def GET(self, CP):
      Final_url = base_url + "appid=" + API_key + "&q=" + CP
 
+     #content = json.loads(weather_data.content.decode('utf-8'))
      weather_data = requests.get(Final_url).json()
      weather_data.get('main').get('temp')
      Meteo = 'meteo : ' + str(weather_data.get('weather')[0].get('main')) + '\ntemp max : ' +str(weather_data.get('main').get('temp_max')) + '\ntemp min : ' + str(weather_data.get('main').get('temp_min')) + '\ntemp actuelle : ' + str(weather_data.get('main').get('temp'))
